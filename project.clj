06fd6449e613eff3c6ad9f1c4ed844bc37f7506a
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2017-06-18 15:20:49 +0200

    Update scm url and clojure

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject apiai-clj "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :scm {:url "git@github.com:MrOerni/apiai-clj.git"}
+  :deploy-branches ["master"]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]])
