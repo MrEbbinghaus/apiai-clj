832aa2f671a5f42cf5b2bbdc095188565106b8b9
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2017-07-02 23:53:58 +0200

    Move doc strings to right positions

--- src/apiai/integrations/facebook.clj
+++ src/apiai/integrations/facebook.clj
@@ -3,23 +3,26 @@
   (:require [apiai.core :as core]
             [clojure.tools.logging :as log]))
 
-(defn response [message]
+(defn response
   "Returns a response for api.ai to forward to the facebook integration.
   message has to be a 'message'-object
   https://developers.facebook.com/docs/messenger-platform/send-api-reference#message"
+  [message]
   {:data {:facebook message}})
 
-(defn quick-reply-response [text quick-replies]
+(defn quick-reply-response
   "Returns a quick reply response for the facebook api.ai integration
   quick-replies has to be a sequence of 'quick_replie's
   https://developers.facebook.com/docs/messenger-platform/send-api-reference/quick-replies#quick_reply"
+  [text quick-replies]
   (response {:text text
              :quick_replies (vec quick-replies)}))
 
-(defn simple-list-response [entrys]
+(defn simple-list-response
   "Returns a response for API.ai to display a simple list in the facebook integration.
   entrys has to be a sequence of 'element' objects
   https://developers.facebook.com/docs/messenger-platform/send-api-reference/list-template#element"
+  [entrys]
   (response {:attachment {:type "template"
                           :payload
                           {:template_type "list"
@@ -29,8 +32,9 @@
                                       :type "postback"
                                       :payload "more"}]}}}))
 
-(defn text-quick-reply [text payload]
+(defn text-quick-reply
   "Returns a quick_reply object of type 'text', with text and payload set"
+  [text payload]
   {:content_type "text"
    :title text
    :payload payload})
