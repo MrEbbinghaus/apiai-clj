15ba1608fda7a597f16b88057cd65bcb90d4c87d
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2017-07-04 00:40:12 +0200

    Update specs

--- src/apiai/integrations/facebook.clj
+++ src/apiai/integrations/facebook.clj
@@ -10,13 +10,14 @@
   [message]
   {:data {:facebook message}})
 
+(defn text-response [text]
+  (response {:text text}))
+
 (defn quick-reply-response
   "Returns a quick reply response for the facebook api.ai integration
-  quick-replies has to be a sequence of 'quick_replie's
+  quick-replies has to be a sequence of 'quick_reply's
   https://developers.facebook.com/docs/messenger-platform/send-api-reference/quick-replies#quick_reply"
   [text quick-replies]
-  {:pre [(s/valid? (s/coll-of ::quick-reply :min-count 1 :max-count 11) quick-replies)
-         (s/valid? string? text)]}
   (response {:text text
              :quick_replies (vec quick-replies)}))
 
@@ -24,15 +25,17 @@
   "Returns a response for API.ai to display a simple list in the facebook integration.
   entrys has to be a sequence of 'element' objects
   https://developers.facebook.com/docs/messenger-platform/send-api-reference/list-template#element"
-  [entrys]
-  (response {:attachment {:type "template"
-                          :payload
-                          {:template_type "list"
-                           :top_element_style "compact"
-                           :elements (vec entrys)
-                           :buttons [{:title "View More"
-                                      :type "postback"
-                                      :payload "more"}]}}}))
+  [more? entrys]
+  (let [r {:attachment {:type "template"
+                        :payload
+                              {:template_type "list"
+                               :top_element_style "compact"
+                               :elements (vec entrys)}}}]
+    (response (if more? (assoc-in r [:attachment :payload :buttons]
+                                  [{:title "View More"
+                                    :type "postback"
+                                    :payload "more"}])
+                        r))))
 
 (defn text-quick-reply
   "Returns a quick_reply object of type 'text', with text and payload set"
--- src/apiai/integrations/facebook_spec.clj
+++ src/apiai/integrations/facebook_spec.clj
@@ -1,4 +1,9 @@
 (ns apiai.integrations.facebook-spec
-  (:require [clojure.spec.alpha :as s]))
+  (:require [clojure.spec.alpha :as s]
+            [apiai.integrations.facebook :as fb]))
 
-(s/def ::quick_reply (s/map-of keyword? string?))
+(s/def ::json-string-object (s/map-of keyword? string?))
+
+(s/fdef fb/quick-reply-response
+        :args (s/cat :text string?
+                     :quick-replies (s/coll-of ::json-string-object :min-count 1 :max-count 11)))
